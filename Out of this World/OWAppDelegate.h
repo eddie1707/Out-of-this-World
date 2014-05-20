4acc316dc6f8e53951f7f98a98f5dc8fc91508f4
Edwin Wiersma <Edwin@MacBook-Pro-van-Edwin.local>
2014-05-20 14:07:59 +0200

    Initial Commit

---
+++ Out of this World/OWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OWAppDelegate.h
+//  Out of this World
+//
+//  Created by Edwin Wiersma on 20-05-14.
+//  Copyright (c) 2014 Apps4Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Out of this World/OWViewController.h
@@ -0,0 +1,13 @@
+//
+//  OWViewController.h
+//  Out of this World
+//
+//  Created by Edwin Wiersma on 20-05-14.
+//  Copyright (c) 2014 Apps4Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWViewController : UIViewController
+
+@end
