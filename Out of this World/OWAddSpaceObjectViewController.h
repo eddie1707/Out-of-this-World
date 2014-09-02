9b840b14c05fd61054b801df349079546184d6b5
Edwin Wiersma <eddie1707@live.nl>
2014-09-02 16:24:13 +0200

    third commit

---
+++ Out of this World/OWAddSpaceObjectViewController.h
@@ -0,0 +1,13 @@
+//
+//  OWAddSpaceObjectViewController.h
+//  Out of this World
+//
+//  Created by Edwin Wiersma on 26/08/14.
+//  Copyright (c) 2014 Apps4Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWAddSpaceObjectViewController : UIViewController
+
+@end
